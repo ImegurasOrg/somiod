c46d2e7c8db6723a5d244143234e0cd24274520b
Imeguras <imeguras@teknik.io>
2022-11-11 13:27:02 +0000

    Cleaned a bit

--- Program.cs
+++ Program.cs
@@ -1,25 +1,35 @@
-var builder = WebApplication.CreateBuilder(args);
+public class Program{
+	public static void Main(string[] args){
+		var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+		// Add services to the container.
+		builder.Services.AddControllers();
+		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+		var app = builder.Build();
 
-var app = builder.Build();
+		// Configure the HTTP request pipeline.
+		// Configure the HTTP request pipeline.
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI();
+		}
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+		//app.UseHttpsRedirection();
+		//add cors permissive
+		app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+		
+		//TOCHECK: is this needed? this might cause problems!
+		app.UseHttpsRedirection();
+
+		app.UseAuthorization();
+
+		app.MapControllers();
+
+		app.Run();
+	}
+	
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
