964c391deb342cf46b00766df1d7fb09107093a5
Imeguras <imeguras@teknik.io>
2022-11-24 19:19:50 +0000

    Werks

--- Controllers/ApplicationController.cs
+++ Controllers/ApplicationController.cs
@@ -13,18 +13,22 @@ namespace somiod.Controllers{
 		
 		//Get all applications
 		[HttpGet]
+		[Produces("application/xml")]
 		public IActionResult Get(){
 			var applications = _context.Aplications;
 			return Ok(applications);
 		}
 		//Get application by id
 		[HttpGet("{id}")]
+		[Produces("application/xml")]
 		public IActionResult Get(int id){
 			var application = _context.Aplications.Find(id);
 			return Ok(application);
 		}
 		//Create application
 		[HttpPost]
+		[Produces("application/xml")]
+		[Consumes("application/xml")]
 		public IActionResult Post([FromBody] Application application){
 			_context.Aplications.Add(application);
 			_context.SaveChanges();
@@ -32,6 +36,8 @@ namespace somiod.Controllers{
 		}
 		//Update application
 		[HttpPut("{id}")]
+		[Produces("application/xml")]
+		[Consumes("application/xml")]
 		public IActionResult Put(int id, [FromBody] Application application){
 			_context.Aplications.Update(application);
 			_context.SaveChanges();
@@ -39,13 +45,15 @@ namespace somiod.Controllers{
 		}
 		//Delete application
 		[HttpDelete("{id}")]
+		[Produces("application/xml")]
+		[Consumes("application/xml")]
 		public IActionResult Delete(int id){
 			var application = _context.Aplications.Find(id);
 			_context.Aplications.Remove(application);
 			_context.SaveChanges();
 			return Ok(application);
 		}
-		
+
 
 	}
 }
--- Program.cs
+++ Program.cs
@@ -18,27 +18,29 @@ namespace somiod{
 	public class Program{
 		public static void Main(string[] args){
 			var builder = WebApplication.CreateBuilder(args);
-
+			
 			// Add services to the container.
 			builder.Services.AddDbContext<InheritanceMappingContext>();
-			builder.Services.AddControllers();
+			//quick plug on the enabling of xml stuff
+			builder.Services.AddControllers().AddXmlSerializerFormatters();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
+			
 			var app = builder.Build();
-
-			// Configure the HTTP request pipeline.
+		
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
 			{
 				app.UseSwagger();
 				app.UseSwaggerUI();
 			}
+			//consume XML, produces XML
+			
 
-			//app.UseHttpsRedirection();
 			//add cors permissive
 			app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-			
+		
 			//TOCHECK: is this needed? this might cause problems!
 			app.UseHttpsRedirection();
 
@@ -48,6 +50,6 @@ namespace somiod{
 
 			app.Run();
 		}
-		
+
 	}
 }
