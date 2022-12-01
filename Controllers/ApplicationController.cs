71531d674060b24f5bc92ac4ffe436634f5106a6
Imeguras <imeguras@teknik.io>
2022-12-01 16:00:44 +0000

    patching routes

--- Controllers/ApplicationController.cs
+++ Controllers/ApplicationController.cs
@@ -4,7 +4,7 @@ using somiod.Models;
 
 namespace somiod.Controllers{
 	[ApiController]
-	[Route("api/[controller]")]
+	[Route("api/somiod/[controller]")]
 	public class ApplicationController : ControllerBase{
 		private readonly InheritanceMappingContext _context;
 		public ApplicationController(InheritanceMappingContext context){
