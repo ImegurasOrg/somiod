e9d961e3a8ad9eb59ad2246d450a476ea8a74112
Imeguras <imeguras@teknik.io>
2022-12-01 12:34:43 +0000

    added api prefix

--- Controllers/DataController.cs
+++ Controllers/DataController.cs
@@ -5,7 +5,7 @@ using somiod.Models;
 namespace somiod.Controllers{
     
     [ApiController]
-	[Route("[controller]")]
+	[Route("api/[controller]")]
 	public class DataController: ControllerBase{
 		private readonly InheritanceMappingContext _context;
 		public DataController(InheritanceMappingContext context){
