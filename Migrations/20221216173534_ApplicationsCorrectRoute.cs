4c302f97b501901244e8aa8b8e61139c0e19fde0
Imeguras <imeguras@teknik.io>
2022-12-21 14:50:29 +0000

    Migration

---
+++ Migrations/20221216173534_ApplicationsCorrectRoute.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace somiod.Migrations
+{
+    public partial class ApplicationsCorrectRoute : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "Applications",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "creation_dt",
+                table: "Applications",
+                type: "datetime(6)",
+                nullable: true,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_name",
+                table: "Applications",
+                column: "name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Applications_name",
+                table: "Applications");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "Applications",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldMaxLength: 20)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "creation_dt",
+                table: "Applications",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true,
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+        }
+    }
+}
